cf30c477597d8d7c65897bf206e4c4311a0ad8d5
Matthew Pennell <mwpennell@gmail.com>
2019-11-25 16:01:44 -0800

    major update to code -- now with finalish plots

--- R/PDR-fitting.R
+++ R/PDR-fitting.R
@@ -3,10 +3,10 @@
 ## using non-homogeneous grid size so density is higher closer to the present
 library(castor)
 
-fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size=4, age0=0,
-                                  ntry_fit=4, ntry_search=1,
-                                  nboot=100, ntry_boot=1, nthreads=1,
-                                  to_return="sim"){
+fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size, age0=0,
+                                  ntry_fit, ntry_search,
+                                  nboot, ntry_boot, nthreads, 
+                                  max_time, to_return="sim"){
   max_age <- get_tree_span(tree)$max_distance
   n_grid <- starting_grid_size
   LTT <- count_lineages_through_time(tree, Ntimes=1000)
@@ -16,14 +16,14 @@ fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size=4, age0=0,
   age_grid_i[1] <- 0
   f_i <- fit_hbd_pdr_on_grid(tree, age_grid = age_grid_i, age0=age0, Ntrials = ntry_search,
                              min_PDR = -5, max_PDR=5, Nthreads = nthreads, condition = "crown",
-                             max_model_runtime = max(0.5,length(tree$tip.label)/ 5e4))
+                             max_model_runtime = max_time)
   age_grid_j <- castor:::get_inhomogeneous_grid_1D(Xstart=0.000001, Xend=max_age, 
                                                    Ngrid=n_grid+1, densityX=rev(max_age-LTT$times),
                                                    densityY=sqrt(rev(LTT$lineages)))
   age_grid_j[1] <- 0
   f_j <- fit_hbd_pdr_on_grid(tree, age_grid = age_grid_j, age0=age0, Ntrials = ntry_search,
                              min_PDR = -5, max_PDR=5, Nthreads = nthreads, condition = "crown",
-                             max_model_runtime = max(0.5,length(tree$tip.label)/ 5e4))
+                             max_model_runtime = max_time)
   aic_i <- f_i$AIC
   aic_j <- f_j$AIC
   
@@ -37,7 +37,7 @@ fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size=4, age0=0,
     age_grid_j[1] <- 0
     f_j <- fit_hbd_pdr_on_grid(tree, age_grid = age_grid_j, age0=age0, Ntrials = ntry_search,
                                min_PDR = -5, max_PDR=5, Nthreads = nthreads, condition = "crown",
-                               max_model_runtime = max(0.5,length(tree$tip.label)/ 5e4))
+                               max_model_runtime = max_time)
     aic_j <- f_j$AIC
   }
   ## use the estimated age grid 
@@ -50,7 +50,7 @@ fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size=4, age0=0,
     f <- fit_hbd_pdr_on_grid(tree, age_grid = age_grid, age0=age0, Ntrials = ntry_fit,
                              min_PDR = -5, max_PDR=5, Nthreads = nthreads, condition = "crown",
                              Nbootstraps = 0, Ntrials_per_bootstrap = ntry_boot,
-                             max_model_runtime = max(0.5,length(tree$tip.label)/ 5e4))
+                             max_model_runtime = max_time)
     if (to_return == "fit"){
       return(list(fit=f)) ## return just the fitted object
     } else {
@@ -64,18 +64,18 @@ fit_pdr_variable_grid <- function(tree, rho=1, starting_grid_size=4, age0=0,
     f_boot <- fit_hbd_pdr_on_grid(tree, age_grid = age_grid, age0=age0, Ntrials = ntry_fit,
                                   min_PDR = -5, max_PDR=5, Nthreads = nthreads, condition = "crown",
                                   Nbootstraps = nboot, Ntrials_per_bootstrap = ntry_boot,
-                                  max_model_runtime = max(0.5,length(tree$tip.label)/ 5e4))
+                                  max_model_runtime = max_time)
     if (to_return == "fit"){
       return(list(fit=f_boot)) ## return just the fitted object
     } else {
       ntip <- length(tree$tip.label)
-      pdr_mle <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age, 
+      pdr_mle <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age, rho=rho,
                                             age_grid = f_boot$age_grid, lambda0=f_boot$fitted_rholambda0/rho,
                                             mu=0, PDR=f_boot$fitted_PDR, splines_degree = 1)
-      pdr_lower <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age,
+      pdr_lower <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age,rho=rho,
                                               age_grid = f_boot$age_grid, lambda0=f_boot$CI95lower$rholambda0/rho,
                                               mu=0, PDR=f_boot$CI95lower$PDR, splines_degree = 1)
-      pdr_upper <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age,
+      pdr_upper <- simulate_deterministic_hbd(LTT0=ntip, oldest_age=max_age, rho=rho,
                                               age_grid = f_boot$age_grid, lambda0=f_boot$CI95upper$rholambda0/rho,
                                               mu=0, PDR=f_boot$CI95upper$PDR, splines_degree = 1)
       ## return the fitted object, the imputed pdr and the imputed CIs
--- R/plots.R
+++ R/plots.R
@@ -2,171 +2,342 @@
 library(ggplot2)
 library(cowplot)
 library(castor)
+library(ggpubr)
 
 ## Functions for making plots
 
 ## Define universal variables
 cols <- c("#5475BA", "#2E3C61")
 
-d <- "output/pdr_boot/"
-pdr_data <- vector(mode = "list", length = length(dir(d)))
-for (i in 1:length(dir(d))){
-  pdr_data[[i]] <- readRDS(paste0(d, dir(d)[i]))
+
+
+## Figure 1: PDR through time -- megaphylogenies only
+
+mega_phylo <- c("Mammals", "Birds", "Squamates", "Amphibians",
+                "Fish", "Chondrichthyans", "Agaricomycetes",
+                "Vascular Plants", "Bacteria")
+
+## read in vrm boot trees
+d_vrm_boot <- "output/vrm_pdr_boot/"
+mega_id <- dir(d_vrm_boot)[sapply(mega_phylo, function(x) grep(x, dir(d_vrm_boot)))]
+vrm_tt <- vector(mode = "list", length = length(mega_id))
+
+for (i in 1:length(mega_id)){
+  vrm_tt[[i]] <- readRDS(paste0(d_vrm_boot, mega_id[i]))
 }
-clade_names <- as.character(sapply(dir(d), function(x) 
-  return(strsplit(x, ".", fixed=TRUE)[[1]][1])))
-names(pdr_data) <- clade_names
+
+names(vrm_tt) <- mega_phylo
+
+trim_sims <- function(x, out=10000){
+  samp_mle <- round(seq(1, length(x$pdr_mle$PDR), length.out = out))
+  x$pdr_mle$PDR <- x$pdr_mle$PDR[samp_mle]
+  x$pdr_mle$LTT <- x$pdr_mle$LTT[samp_mle]
+  x$pdr_mle$ages <- x$pdr_mle$ages[samp_mle]
+  samp_up <- seq(1, length(x$pdr_upper$PDR), length.out = out)
+  x$pdr_upper$PDR <- x$pdr_upper$PDR[samp_up]
+  x$pdr_upper$LTT <- x$pdr_upper$LTT[samp_up]
+  x$pdr_upper$ages <- x$pdr_upper$ages[samp_up]
+  samp_lo <- seq(1, length(x$pdr_lower$PDR), length.out = out)
+  x$pdr_lower$PDR <- x$pdr_lower$PDR[samp_lo]
+  x$pdr_lower$LTT <- x$pdr_lower$LTT[samp_lo]
+  x$pdr_lower$ages <- x$pdr_lower$ages[samp_lo]
+  x
+}
+
+vrm_tt <- lapply(vrm_tt, function(x) trim_sims(x))
 
 
-## Plot 1 : PDR through time (combine later)
-pdr_time <- function(res, clade_name, cols=c("#5475BA", "#2E3C61")){
+pdr_time_boot <- function(res, clade_name, cols){
   df <- data.frame(PDR=c(res$pdr_lower$PDR, res$pdr_upper$PDR, res$pdr_mle$PDR), 
                    type=c(rep("lower", length(res$pdr_lower$PDR)), rep("upper", length(res$pdr_upper$PDR)), rep("mle", length(res$pdr_mle$PDR))),
                    ages=c(res$pdr_lower$ages, res$pdr_upper$ages, res$pdr_mle$ages))
-  ggplot(df, aes(x=ages, y=PDR, color=type)) + stat_smooth(se=FALSE) + 
-    scale_colour_manual(values = c("#5475BA", "#2E3C61", "#5475BA")) + 
+  ggplot(df, aes(x=ages, y=PDR, color=type)) + geom_line() + #stat_smooth(se=FALSE) + 
+    scale_colour_manual(values = c(cols[1], cols[2], cols[1])) + 
     theme_cowplot() + theme(legend.position = "none") + 
-    xlab("Time before present (My)") + ylab(expression(r[p]))
-  ggsave(paste0("figs/pdr_time/", clade_name, ".pdf"))
+    xlab("Time before present (My)") + ylab(expression(r[p])) +
+    labs(title = clade_name) +
+    theme(axis.title.x = element_text(size=8), axis.title.y = element_text(size=8),
+          axis.text = element_text(size=6), plot.title = element_text(size=8))
 }
 
-lapply(1:length(pdr_data), function(x) 
-  pdr_time(pdr_data[[x]], clade_name = names(pdr_data)[x]))
+
+figs_1 <- lapply(1:length(vrm_tt), function(x) 
+  pdr_time_boot(vrm_tt[[x]], clade_name=names(vrm_tt)[x], cols=cols))
+
+ggarrange(figs_1[[1]], figs_1[[2]], figs_1[[3]],
+          figs_1[[4]], figs_1[[5]], figs_1[[6]],
+          figs_1[[7]], figs_1[[8]], figs_1[[9]], 
+          ncol=3, nrow=3)
+
+ggsave("figs/ms/pdr_time_megaphylo.pdf", width=7, height=6) ## need to adjust proportions
 
 
-## Plot 2: PSR through time
-psr_time <- function(res, clade_name, cols=c("#5475BA", "#2E3C61")){
-  df <- data.frame(PSR=c(res$pdr_lower$PSR, res$pdr_upper$PSR, res$pdr_mle$PSR), 
-                   type=c(rep("lower", length(res$pdr_lower$PSR)), rep("upper", length(res$pdr_upper$PSR)), rep("mle", length(res$pdr_mle$PSR))),
-                   ages=c(res$pdr_lower$ages, res$pdr_upper$ages, res$pdr_mle$ages))
-  ggplot(df, aes(x=ages, y=PSR, color=type)) + stat_smooth(se=FALSE) + 
-    scale_colour_manual(values = c("#5475BA", "#2E3C61", "#5475BA")) + 
-    theme_cowplot() + theme(legend.position = "none") + 
-    xlab("Time before present (My)") + ylab(expression(lambda[p]))
-  ggsave(paste0("figs/psr_time/", clade_name, ".pdf"))
+
+
+
+
+
+
+## Figure 2: PDR through time -- everything else (Henao Diaz trees)
+## cut out Seed Plants for the time being
+mega_id_sp <- c(mega_id, "Seed Plants.rds")
+hd_id <- dir(d_vrm_boot)[-which(dir(d_vrm_boot) %in% mega_id_sp)]
+vrm_tt_supp <- vector(mode = "list", length = length(hd_id))
+clade_names_supp <- vector(length=length(hd_id))
+
+for (i in 1:length(hd_id)){
+  vrm_tt_supp[[i]] <- readRDS(paste0(d_vrm_boot, hd_id[i]))
+  clade_names_supp[i] <- as.character(strsplit(hd_id[i], ".", fixed=TRUE)[[1]][1])
 }
 
-lapply(1:length(pdr_data), function(x) 
-  psr_time(pdr_data[[x]], clade_name = names(pdr_data)[x]))
+names(vrm_tt_supp) <- clade_names_supp
+
+vrm_tt_supp <- lapply(vrm_tt_supp, function(x) trim_sims(x))
+
+figs_s1 <- lapply(1:20, function(x) 
+  pdr_time_boot(vrm_tt_supp[[x]], clade_name=names(vrm_tt_supp)[x], cols=cols))
+
+## Break figure up into two
+ggarrange(figs_s1[[1]], figs_s1[[2]], figs_s1[[3]],
+          figs_s1[[4]], figs_s1[[5]], figs_s1[[6]],
+          figs_s1[[7]], figs_s1[[8]], figs_s1[[9]],
+          figs_s1[[10]],
+          figs_s1[[11]], figs_s1[[12]], figs_s1[[13]],
+          figs_s1[[14]], figs_s1[[15]], figs_s1[[16]],
+          figs_s1[[17]], figs_s1[[18]], figs_s1[[19]],
+          figs_s1[[20]], 
+          ncol=5, nrow=4)
+
+ggsave("figs/ms/pdr_time_henaodiaz1.pdf", width=11, height=8.5) ## need to adjust proportions
+
+figs_s1b <- lapply(21:39, function(x) 
+  pdr_time_boot(vrm_tt_supp[[x]], clade_name=names(vrm_tt_supp)[x], cols=cols))
+
+ggarrange(figs_s1b[[1]], figs_s1b[[2]], figs_s1b[[3]],
+         figs_s1b[[4]], figs_s1b[[5]], figs_s1b[[6]],
+         figs_s1b[[7]], figs_s1b[[8]], figs_s1[[9]],
+         figs_s1b[[10]],
+         figs_s1b[[11]], figs_s1b[[12]], figs_s1b[[13]],
+         figs_s1b[[14]], figs_s1b[[15]], figs_s1b[[16]],
+         figs_s1b[[17]], figs_s1b[[18]], figs_s1b[[19]],
+         ncol=5, nrow=4)
+
+ggsave("figs/ms/pdr_time_henaodiaz2.pdf", width=11, height=8.5) ## need to adjust proportions
 
 
-## Plot 3: Lambda0 vs. clade age
-
-## grab rho from each dataset from data table
-tree_info <- read.csv("data/tree_descriptions.csv")
-rhos <- tree_info$rho
-names(rhos) <- tree_info$taxon
-rhos <- rhos[clade_names]
-rhos <- rhos[!is.na(rhos)]
-
-## build a dataframe with all the information
-lambda0ml <- sapply(pdr_data, function(x) return(x$fit$fitted_rholambda0))
-lambda0lo <- sapply(pdr_data, function(x) return(x$fit$CI95lower$rholambda0))
-lambda0up <- sapply(pdr_data, function(x) return(x$fit$CI95upper$rholambda0))
-lambda0se <- sapply(pdr_data, function(x) return(x$fit$standard_errors$rholambda0))
-rp0ml <- sapply(pdr_data, function(x) return(x$fit$fitted_PDR[1]))
-rp0lo <- sapply(pdr_data, function(x) return(x$fit$CI95lower$PDR[1]))
-rp0up <- sapply(pdr_data, function(x) return(x$fit$CI95upper$PDR[1]))
-rp0se <- sapply(pdr_data, function(x) return(x$fit$standard_errors$PDR[1]))
-age <- sapply(pdr_data, function(x) return(max(x$fit$age_grid)))
-
-pdr_df <- data.frame(lambda0rho = lambda0ml, lambda0rho_lo = lambda0lo,
-                     lambda0rho_up = lambda0up, lambda0rho_se = lambda0se,
-                     rp0 = rp0ml, rp0lo = rp0lo, rp0up = rp0up, rp0se = rp0se, 
-                     rho = rhos, age=age, clade=clade_names)
-## solve for lambda0 given rho
-pdr_df$lambda0 <- pdr_df$lambda0rho / pdr_df$rho
-## calculate mu_p0 given lambda0 and rho
-pdr_df$mu_p0 <- pdr_df$lambda0 - pdr_df$rp0
-
-## Exclude bacteria since it is so much older
-pdr_df_nb <- pdr_df[-which(pdr_df$clade == "Bacteria"), ]
-
-ggplot(pdr_df_nb, aes(x=age, y=lambda0)) +  
-  geom_errorbar(aes(ymin=lambda0-lambda0se, ymax=lambda0+lambda0se)) +
-  geom_point(colour="#5475BA", size=3, alpha=0.7) + 
-  ylab(expression(lambda(0))) + xlab("Age of clade (my)") + 
-  theme_cowplot() #+ stat_smooth(se=FALSE, colour="#2E3C61")
-ggsave("figs/clade_age/lambda0_cladeage.pdf")
 
 
-## Plot 4: rp0 vs clade age (standard scale)
-## Exclude bacteria since it is so much older than the rest
-
-ggplot(pdr_df_nb, aes(x=age, y=rp0)) +  
-  geom_errorbar(aes(ymin=rp0-rp0se, ymax=rp0+rp0se)) +
-  geom_point(colour="#5475BA", size=3, alpha=0.7) + 
-  ylab(expression(r[p](0))) + xlab("Age of clade (my)") + 
-  theme_cowplot() + stat_smooth(se=FALSE, colour="#2E3C61")
-ggsave("figs/clade_age/rp0_cladeage.pdf")
 
 
-## Plot 6: mu_p0 vs. epislon from constant rate model
-tmp <- grep(pattern="0_", x=dir("output/srm"))
-ds <- "output/srm/"
-srm_data <- vector(mode = "list", length = length(dir(ds)[tmp]))
-for (i in 1:length(dir(ds)[tmp])){
-  srm_data[[i]] <- readRDS(paste0(ds, dir(ds)[i]))
-}
-clade_names_srm <- as.character(sapply(dir(ds)[tmp], function(x) 
-  return(strsplit(x, "_", fixed=TRUE)[[1]][2])))
-clade_names_srm <- as.character(sapply(clade_names_srm, function(x) 
-  return(strsplit(x, ".", fixed=TRUE)[[1]][1])))
-names(srm_data) <- clade_names_srm
-
-## Compute epsilon
-lambda0ml_srm <- sapply(srm_data, function(x) return(x$fit$fitted_rholambda0))
-#lambda0lo_srm <- sapply(srm_data, function(x) return(x$fit$CI95lower$rholambda0))
-#lambda0up_srm <- sapply(srm_data, function(x) return(x$fit$CI95upper$rholambda0))
-#lambda0se_srm <- sapply(srm_data, function(x) return(x$fit$standard_errors$rholambda0))
-rp0ml_srm <- sapply(srm_data, function(x) return(x$fit$fitted_PDR[1]))
-#rp0lo_srm <- sapply(srm_data, function(x) return(x$fit$CI95lower$PDR[1]))
-#rp0up_srm <- sapply(srm_data, function(x) return(x$fit$CI95upper$PDR[1]))
-#rp0se_srm <- sapply(srm_data, function(x) return(x$fit$standard_errors$PDR[1]))
-#age_srm <- sapply(srm_data, function(x) return(max(x$fit$age_grid)))
-
-srm_df <- data.frame(lambda0rho_srm = lambda0ml_srm, rp0_srm = rp0ml_srm, 
-                     rho = rhos[clade_names_srm], clade=clade_names_srm)
-## compute lambda0
-srm_df$lambda0_srm <- srm_df$lambda0rho_srm / srm_df$rho
-## compute epsilon
-srm_df$mu0 <- srm_df$lambda0_srm - srm_df$rp0_srm
-srm_df$eps <- srm_df$mu / srm_df$lambda0_srm
-
-## Plot 7: LTT vs. dLTT plots
-ltt_dltt <- function(tree, fit, cols=c("#5475BA", "#2E3C61")){
+## Figure 3 and 4: Comparison of LTT and dLTT plots
+fig_dltt_ltt <- function(tree, dltt, clade_name, cols){
   tree_age <- get_tree_span(tree)$max_distance
-  ltt <- castor::count_lineages_through_time(tree, times=seq(0, (tree_age - 0.1), length.out = 100))
-  dltt <- fit$pdr_mle
-  min_one_lineage <- which(dltt$LTT >= 1)
-  dlineages <- dltt$LTT[min_one_lineage]
-  dages <- tree_age - dltt$ages[min_one_lineage]
-  ltt_comp <- data.frame(lineages=c(ltt$lineages, dlineages),
-                         ages=c(ltt$times, dages),
-                         Type=c(rep("LTT", length(ltt$lineages)),
-                                rep("dLTT", length(dlineages))))
+  ## get the ltt at every point
+  
+  ltt <- castor::count_lineages_through_time(tree, Ntimes=1000)
+  lineages <- ltt$lineages[1:(length(ltt$lineages) - 1)]
+  lineage_times <- ltt$times[1:(length(ltt$times) - 1)]
+                                             
+  ltt_comp <- data.frame(lineages=c(lineages, rev(dltt$LTT)),
+                         ages=c(lineage_times, dltt$ages),
+                         Type=c(rep("LTT", length(lineages)),
+                                rep("dLTT", length(dltt$LTT))))
   ltt_comp$ages <- max(ltt_comp$ages) - ltt_comp$ages
   
+  
   ggplot(ltt_comp, aes(x=ages, y=lineages, colour=Type)) + 
-    stat_smooth(se=FALSE) + scale_y_log10() + theme_cowplot() +
-    scale_colour_manual(values = cols) 
-    ##theme(legend.position = "none", axis.title = element_blank(),
-    ##      axis.text = element_text(size=9))
+    geom_line() + scale_y_log10() + theme_cowplot() +
+    scale_colour_manual(values = cols) + labs(title = clade_name) +
+    xlab("Time before present (My)") + ylab("Lineages") +
+    theme(axis.title.x = element_text(size=7), axis.title.y = element_text(size=7),
+          axis.text = element_text(size=6), plot.title = element_text(size=8),
+          legend.position = "none")
 }
+
+fig_dltt_ltt_small <- function(tree, dltt, clade_name, cols){
+  tree_age <- get_tree_span(tree)$max_distance
+  ## get the ltt at every point
+  
+  ltt <- castor::count_lineages_through_time(tree, Ntimes=1000)
+  lineages <- ltt$lineages[1:(length(ltt$lineages) - 1)]
+  lineage_times <- ltt$times[1:(length(ltt$times) - 1)]
+  
+  ltt_comp <- data.frame(lineages=c(lineages, rev(dltt$LTT)),
+                         ages=c(lineage_times, dltt$ages),
+                         Type=c(rep("LTT", length(lineages)),
+                                rep("dLTT", length(dltt$LTT))))
+  ltt_comp$ages <- max(ltt_comp$ages) - ltt_comp$ages
+  
+  
+  ggplot(ltt_comp, aes(x=ages, y=lineages, colour=Type)) + 
+    geom_line() + scale_y_log10() + theme_cowplot() +
+    scale_colour_manual(values = cols) + labs(title = clade_name) +
+    xlab("Mya") + ylab("Lineages") +
+    theme(axis.title.x = element_text(size=5), axis.title.y = element_text(size=5),
+          axis.text = element_text(size=5), plot.title = element_text(size=6),
+          legend.position = "none")
+}
+
 
 tree_info <- read.csv("data/tree_descriptions.csv")
 
-for (i in 1:nrow(tree_info)){
-  id <- as.character(tree_info[i,"tree_name"])
-  rho <- as.numeric(tree_info[i,"rho"])
-  clade <- as.character(tree_info[i, "taxon"])
-  tree <- castor::read_tree(file=paste0("data/trees/", id, ".tre"))
-  fit <- pdr_data[[clade]]
-  ltt_dltt(tree, fit)
-  ggsave(paste0("figs/ltt/ltt-dltt-", clade, ".pdf"))
+
+
+## 3. Megaphylogenies only
+
+tree_info_mega <- tree_info[which(tree_info$taxon %in% mega_phylo), ]
+tree_list_mega <- as.character(tree_info_mega$tree_name)
+names(tree_list_mega) <- as.character(tree_info_mega$taxon)
+## reorder
+tree_list_mega <- tree_list_mega[mega_phylo]
+
+
+dltt_wrapper <- function(i, tree_list, dltt_out){
+  t <- read_tree(file=paste0("data/trees/", 
+                             as.character(tree_list[i]), ".tre"))
+  clade <- as.character(names(tree_list)[i])
+  f <- dltt_out[[clade]]$pdr_mle
+  list(tree=t, dltt=f, clade=clade)
 }
 
 
-## Figure 7: epsilon vs. mu_0  
+figs_3 <- lapply(c(1:length(tree_list_mega)), function(x) {
+  tmp <- dltt_wrapper(x, tree_list_mega, vrm_tt)
+  fig_dltt_ltt(tmp$tree, dltt=tmp$dltt, clade_name=tmp$clade, cols=cols)
+})
 
-## 1. compute mu_0 from fitted rp and lambda_0
+ggarrange(figs_3[[1]], figs_3[[2]], figs_3[[3]],
+          figs_3[[4]], figs_3[[5]], figs_3[[6]],
+          figs_3[[7]], figs_3[[8]], figs_3[[9]],
+          ncol=3, nrow=3)
+
+ggsave("figs/ms/ltt_megaphylo.pdf", width=8, height=6) ## need to adjust proportions
+
+
+
+## Figure 4: Comparison of LTT and dLTT plots
+## Henao Diaz trees only
+## remove: Seedplants 
+mega_phylo_sp <- c(mega_phylo, "Seed Plants")
+tree_info_hd <- tree_info[-which(tree_info$taxon %in% mega_phylo_sp), ]
+tree_list_hd <- as.character(tree_info_hd$tree_name)
+names(tree_list_hd) <- as.character(tree_info_hd$taxon)
+## reorder
+tree_list_hd <- tree_list_hd[clade_names_supp]
+
+figs_s2 <- lapply(c(1:20), function(x) {
+  tmp <- dltt_wrapper(x, tree_list_hd, vrm_tt_supp)
+  fig_dltt_ltt_small(tmp$tree, dltt=tmp$dltt, clade_name=tmp$clade, cols=cols)
+})
+
+## Break figure up into two
+ggarrange(figs_s2[[1]], figs_s2[[2]], figs_s2[[3]],
+          figs_s2[[4]], figs_s2[[5]], figs_s2[[6]],
+          figs_s2[[7]], figs_s2[[8]], figs_s2[[9]],
+          figs_s2[[10]],
+          figs_s2[[11]], figs_s2[[12]], figs_s2[[13]],
+          figs_s2[[14]], figs_s2[[15]], figs_s2[[16]],
+          figs_s2[[17]], figs_s2[[18]], figs_s2[[19]],
+          figs_s2[[20]], 
+          ncol=5, nrow=4)
+
+ggsave("figs/ms/ltt_henaodiaz1.pdf", width=11, height=8.5) ## need to adjust proportions
+
+figs_s2b <- lapply(c(21:39), function(x) {
+  tmp <- dltt_wrapper(x, tree_list_hd, vrm_tt_supp)
+  fig_dltt_ltt_small(tmp$tree, dltt=tmp$dltt, clade_name=tmp$clade, cols=cols)
+})
+
+ggarrange(figs_s2b[[1]], figs_s2b[[2]], figs_s2b[[3]],
+         figs_s2b[[4]], figs_s2b[[5]], figs_s2b[[6]],
+         figs_s2b[[7]], figs_s2b[[8]], figs_s2b[[9]],
+         figs_s2b[[10]],
+         figs_s2b[[11]], figs_s2b[[12]], figs_s2b[[13]],
+         figs_s2b[[14]], figs_s2b[[15]], figs_s2b[[16]],
+         figs_s2b[[17]], figs_s2b[[18]], figs_s2b[[19]],
+         ncol=5, nrow=4)
+
+ggsave("figs/ms/ltt_henaodiaz2.pdf", width=11, height=8.5) ## need to adjust proportions
+
+
+
+
+
+
+
+## Figure 5: mu_p(x) vs. epsilon* 
+
+
+## function for extracting pulled extinction rates at time t 
+get_pulled_ext <- function(f, rho) {
+  lambda_t <- f$fit$fitted_rholambda0 / rho
+  rp_t <- f$fit$fitted_PDR[1]
+  lambda_t - rp_t
+}
+
+## function for estimating epislon*
+get_epsilon <- function(f){
+  f$fitted_mu[1] / f$fitted_lambda[1]
+}
+
+taxa <- as.character(tree_info$taxon)
+## tmp: remove Seed Plants
+sp <- grep("Seed Plants", taxa)
+taxa <- taxa[-sp]
+rhos <- tree_info$rho[-sp]
+
+mup_0 <- rep(NA, length=length(taxa))
+eps_0 <- rep(NA, length=length(taxa))
+## Estimate rp[0] just for additional info
+rp_0 <- rep(NA, length=length(taxa))
+
+for (i in 1:length(taxa)){
+  file_id <- dir("output/vrm_pdr_boot/")[grep(taxa[i], dir("output/vrm_pdr_boot/"))]
+  tmp <- readRDS(paste0("output/vrm_pdr_boot/", file_id))
+  mup_0[i] <- get_pulled_ext(tmp, rhos[i])
+  rp_0[i] <- tmp$fit$fitted_PDR[1]
+  file_id <- dir("output/srm_est_lambda/")[grep(taxa[i], dir("output/srm_fixed_lambda/"))]
+  tmp <- readRDS(paste0("output/srm_est_lambda/", file_id))
+  if (tmp$success == TRUE){
+    eps_0[i] <- get_epsilon(tmp)
+  }
+}
+
+mu_df <- data.frame(taxa= taxa, 
+                    mup=mup_0,
+                    eps=eps_0,
+                    rp_0=rp_0)
+
+## some summary stats for the paper
+## how many mu_p are negative
+
+## for age0
+## absolute numbers
+length(which(mu_df$mup < 0)) 
+## as a percentage
+length(which(mu_df$mup < 0)) / nrow(mu_df)
+
+## summary stats of rp[0]
+length(which(mu_df$rp_0 > 0))
+median(mu_df$rp_0)
+sd(mu_df$rp_0)
+
+ggplot(mu_df, aes(x=mup, y=eps)) + geom_point(colour=cols[1], size=4) + 
+  theme_cowplot() + xlab(expression(mu[p](0))) + 
+  ylab(expression(epsilon)) + 
+  geom_vline(xintercept=0, colour=cols[2],linetype='dashed') + 
+  geom_hline(yintercept=0, colour=cols[2],linetype='dashed') +
+  geom_hline(yintercept=1, colour=cols[2],linetype='dashed') +
+  theme(axis.text = element_text(size=12))
+
+ggsave("figs/ms/epsilon_est_mup_age0.pdf") ## need to fix dimensions
+
+
+
+
+
+
+
+
+
+
